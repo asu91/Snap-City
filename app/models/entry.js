b473640dc386dd8cb574ecd0049329374adbfc24
Alex Su <alexsu91@gmail.com>
2016-02-16 12:05:58 -0800

    tried fiddling with charts. update img

--- app/models/entry.js
+++ app/models/entry.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Entry', {
   // name: {type: String, default: ''},
   // bench: {type: Number, default: 0},
-  squat: {type: Number, default: 0},
-  deadlift: {type: Number, default: 0}
+  squat: {type: Number, default: 0}
+  // deadlift: {type: Number, default: 0}
 });
