4d4b75be2cd0b4c86f0ec433c456000b1500c51a
Alex Su <alexsu91@gmail.com>
2016-02-15 20:49:53 -0800

    gets data from db

--- public/js/controllers/StatsCtrl.js
+++ public/js/controllers/StatsCtrl.js
@@ -1,4 +1,16 @@
 angular.module('mvp.statsCtrl',[])
-  .controller('StatsController', function($scope){
+  .controller('StatsController', function($scope, EntryFactory){
     $scope.tagline = 'HI THERE';
+    $scope.numbers = {};
+    var initialize = function(){
+      EntryFactory.get()
+        .then(function(resp){
+          console.log(resp.data, '<-----respdata')
+          $scope.numbers = resp
+        })
+        .catch(function(err){
+          console.error(err);
+        })
+    }
+    initialize();
   });
