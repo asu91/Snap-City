82d224ba38fc31c1c58de6f597bae8fe1a22fa46
Alex Su <alexsu91@gmail.com>
2016-02-15 16:23:04 -0800

    refactored requests

--- public/js/services/EntryFactory.js
+++ public/js/services/EntryFactory.js
@@ -2,20 +2,40 @@ angular.module('mvp.factory', [])
   .factory('EntryFactory', function($http){
 
     var get = function(){
-      return $http.get('/api/entry');
+      return $http({
+        method: 'GET',
+        url: '/api/entry'
+      })
+      .then(function(resp){
+        return resp.data;
+      });
     };
 
-    var create = function(entryData){
-      return $http.post('/api/entry', entryData);
+    var post = function(entryData){
+      return $http({
+        method: 'POST',
+        url: '/api/entry',
+        data: entryData
+      })
+      .then(function(){
+        console.log('POST SUCCESSFUL');
+      });
     };
 
     var erase = function(id){
-      return $http.delete('/api/entry', id);
+      return $http({
+        method: 'DELETE',
+        url: 'api/entry',
+        data: id
+      })
+      .then(function(){
+        console.log('DELETED');
+      });
     };
 
     return {
       get: get,
-      create: create,
+      post: post,
       erase: erase
     };
     
